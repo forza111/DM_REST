3092a29af77dedebcf81e0a43e33bfe1a5a27952
forza111 <nikitos.ionkin@yandex.ru>
2021-03-11 20:02:13 +0300

    Init ver.4

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django_movie.secret import *
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
