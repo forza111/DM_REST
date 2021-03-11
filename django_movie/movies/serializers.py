70437362e861005060f33b6bc73df1e5eda7e912
forza111 <nikitos.ionkin@yandex.ru>
2021-03-12 00:00:06 +0300

    Добавлене сериализаторов, позволяющих выводить не id категорий, режиссеров, актеров и жанров, а их название (slug_field = name)

--- django_movie/movies/serializers.py
+++ django_movie/movies/serializers.py
@@ -12,6 +12,11 @@ class MovieListSerializer(serializers.ModelSerializer):
 
 class MovieDetailSerializer(serializers.ModelSerializer):
     '''Полный фильм'''
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    directors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    actors = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+    genres = serializers.SlugRelatedField(slug_field="name", read_only=True, many=True)
+
 
     class Meta:
         model = Movie
