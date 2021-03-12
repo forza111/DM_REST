ae4e1e1d995230ca4a38b571c39f5eb2c38c5bbc
forza111 <nikitos.ionkin@yandex.ru>
2021-03-12 23:24:45 +0300

    Рекурсивный вывод влженных записей (отзывов)

--- django_movie/movies/serializers.py
+++ django_movie/movies/serializers.py
@@ -2,10 +2,22 @@ from rest_framework import serializers
 
 from .models import Movie, Review
 
+class FilterReviewListSerializer(serializers.ListSerializer):
+    '''Фильтр комментариев, только parrent'''
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    '''Вывос рекурсивно children'''
+    def to_representation(self, value):#value - значение одной записи из БД
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
 
 class MovieListSerializer(serializers.ModelSerializer):
     '''Список фильмов'''
-
     class Meta:
         model = Movie
         fields = ("title", "tagline", "category")
@@ -13,7 +25,6 @@ class MovieListSerializer(serializers.ModelSerializer):
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
     '''Добавление отзыва'''
-
     class Meta:
         model = Review
         fields = "__all__"
@@ -21,10 +32,12 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     '''Вывод отзывов'''
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ("name", "text", "parent")
+        fields = ("name", "text", "children")
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
