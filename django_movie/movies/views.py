9adcba31b36029e6efb0741ee66d655255c2d134
forza111 <nikitos.ionkin@yandex.ru>
2021-03-13 00:27:21 +0300

    Перенос функции получения IP адреса в отдельный фаил

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -33,14 +33,6 @@ class ReviewCreateView(APIView):
 
 class AddStarRatingView(APIView):
     '''Добавление рейтинга фильму'''
-    def get_client_ip(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
-
     def post(self, request):
         serializer = CreateRatingSerializer(data=request.data)
         if serializer.is_valid():
